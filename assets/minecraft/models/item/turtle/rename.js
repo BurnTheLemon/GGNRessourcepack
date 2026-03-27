f71d54248eaa8426c9d7464453b2cd9fffd3c83b
BurnTheLemon <burnthelemon@gmail.com>
2026-03-27 04:45:59 +0100

    Update

---
+++ assets/minecraft/models/item/turtle/rename.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const folder = process.argv[2] || '.';
+const searchStr = process.argv[3] || 'magmarite';
+const replaceStr = process.argv[4] || 'turtle';
+
+const files = fs.readdirSync(folder);
+
+files.forEach(file => {
+  if (file.includes(searchStr)) {
+    const newName = file.replaceAll(searchStr, replaceStr);
+    fs.renameSync(path.join(folder, file), path.join(folder, newName));
+    console.log(`Renamed: ${file} → ${newName}`);
+  }
+});
+
+console.log('Done!');
